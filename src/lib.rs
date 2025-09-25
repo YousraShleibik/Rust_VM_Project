67482b524cec65689f2afa7015bfa41aee3069e1
yousra shleibik <143129587+YousraShleibik@users.noreply.github.com>
2025-09-24 20:15:35 -0600

    Implement OpCode conversion functions for u8 and vice versa

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,30 @@ pub enum OpCode {
     OpDivide,
 
 }
+
+//helper function to convert OpCode to u8
+pub fn opcode_to_u8(op: OpCode) -> u8 {
+    match op {
+        OpCode::OpReturn   => 0x00,
+        OpCode::OpConstant => 0x01,
+        OpCode::OpNegate   => 0x02,
+        OpCode::OpAdd      => 0x03,
+        OpCode::OpSubtract => 0x04,
+        OpCode::OpMultiply => 0x05,
+        OpCode::OpDivide   => 0x06,
+    }
+}
+
+//helper function to convert u8 to OpCode
+pub fn u8_to_opcode(b: u8) -> Option<OpCode> {
+    Some(match b {
+        0x00 => OpCode::OpReturn,
+        0x01 => OpCode::OpConstant,
+        0x02 => OpCode::OpNegate,
+        0x03 => OpCode::OpAdd,
+        0x04 => OpCode::OpSubtract,
+        0x05 => OpCode::OpMultiply,
+        0x06 => OpCode::OpDivide,
+        _ => return None,
+    })
+}
